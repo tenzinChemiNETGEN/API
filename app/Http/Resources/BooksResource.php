a5e4163ac9ffab08eafa9aa56bdbb0ea16bdb847
tenzinChemiNETGEN <tenzin.chemi@netgensolutions.com>
2021-09-16 17:23:45 +0530

    laravel api

---
+++ app/Http/Resources/BooksResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BooksResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'Books' => [
+                'id' =>(string) $this->id,
+                'type' => 'Books',
+                'attributes' => [
+                    'name' => $this->name,
+                    'author' => $this->author,
+                    'description' => $this->description,
+                    'publication_year' => $this->publication_year,
+                    'created_at' => $this->created_at,
+                    'updated_at' => $this->updated_at,
+                ]
+            ]
+        ];
+    }
+}
+
+
